1ffbf80b3d652a674d0ac109afe3d5392ac32f6a
raynnerz <22.01694-5@maua.br>
2025-06-12 12:36:53 -0300

    repair: flecha de voltar da tela de perfil

--- lib/app/presentation/profile/pages/profile_page.dart
+++ lib/app/presentation/profile/pages/profile_page.dart
@@ -37,9 +37,7 @@ class _ProfilePageState extends State<ProfilePage> {
                   leading: IconButton(
                     icon: const Icon(Icons.arrow_back, color: Colors.black),
                     onPressed: () {
-                      Modular.to.navigate(
-                        '/login/sign-in',
-                      );
+                      Modular.to.pop();
                     },
                   ),
                   actions: [
